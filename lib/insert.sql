78cbd9333ba48b4ffc47aef19b4c899068429064
pleugemors <pleugemors@yahoo.com>
2020-02-25 16:27:14 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (name, age) VALUES 
+('Hayle', 25), ('Paul', 35), ('Justin', 15), ('Jennifer', 27), ('Tina', 30), 
+('Thomas', 28), ('Brandon', 31), ('Tim', 19), ('Arlin', 20), ('Paula', 50), 
+('Juan', 16), ('Julio', 3), ('Jesus', 1056), ('Allah', 1056), ('Eric', 48), 
+('Jim', 55), ('Teresa', 49), ('Janet', 27), ('Heather', 33), ('Tom', 40);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+    ('Fund a Pet', 'adoption', 200.00, '2012-01-27', '2012-02-20'),
+    ('Mentor A Brother', 'Comunity Service', 50.00, '2014-02-06', '2014-03-05'),
+    ('Big Bro', 'Community Service', 100.00, '2014-01-01', '2015-01-01'),
+    ('Bro', 'Service', 200.00, '2014-01-30', '2015-01-21'),
+    ('Big', 'Service', 100.00, '2014-01-12', '2015-01-28'),
+    ('food drive', 'Community Service', 100.00, '2014-01-01', '2015-01-31'),
+    ('Secret Santa', 'Holiday', 100.00, '2014-01-01', '2015-01-01'),
+    ('Meal On Wheels', 'Community Service', 100.00, '2014-01-01', '2015-01-01'),
+    ('Parade decoration', 'Community', 300.00, '2014-01-01', '2015-01-01'),
+    ('Big Bro', 'Community Service', 100.00, '2014-01-01', '2015-01-01');
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+    (24.00, 1, 4), (20.95, 2, 8), (5.00, 3, 1), (10.00, 4, 2), (15.00, 5, 3),
+    (20.00, 6, 5), (25, 7, 6), (30.00, 7, 7), (35.00, 8, 9), (40.00, 10, 10),
+    (24.00, 1, 4), (20.95, 2, 8), (5.00, 3, 1), (10.00, 4, 2), (15.00, 5, 3),
+    (24.00, 1, 4), (20.95, 2, 8), (5.00, 3, 1), (10.00, 4, 2), (15.00, 5, 3),
+    (24.00, 1, 4), (20.95, 2, 8), (5.00, 3, 1), (10.00, 4, 2), (15.00, 5, 3),
+    (24.00, 1, 4), (20.95, 2, 8), (5.00, 3, 1), (10.00, 4, 2), (15.00, 5, 3);
+    
+
+
+
